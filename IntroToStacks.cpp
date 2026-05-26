c45aacea22a38bb756b14502cd84d457dcb5b3dd
[Sandeep] <sandeep977244@gmail.com>
2026-05-26 08:45:01 +0530

    Stack implementation using vectors .

--- IntroToStacks.cpp
+++ IntroToStacks.cpp
@@ -1,59 +1,71 @@
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <vector>
 using namespace std;
 
-
-// create stacks using vectors
+// Creating Stack using Vector
 
 class Stack
 {
-	vector<int> vec;
-	public:
-	push(int val) //0(1)
-	{
-		vec.push_back(val);
-	}
-	
-	void pop()
-	{
-		if(isEmpty())
-		{
-			cout<<"Stack is Empty. "<<endl;
-		}
-		vec.pop_back();
-	}
-	
-	int top()
-	{
-		if(isEmpty())
-		{
-			cout<<"Stack is Empty. "<<endl;
-			return -1;
-		}
-		int lastIdx = vec.size()-1;
-		return vec[lastIdx];
-	}
-	
-	bool isEmpty()
-	{
-		return vec.size() == 0;
-	}
+private:
+    vector<int> vec;   // Vector to store stack elements
+
+public:
+
+    // Push element into stack  -> O(1)
+    void push(int val)
+    {
+        vec.push_back(val);
+    }
+
+    // Remove top element from stack
+    void pop()
+    {
+        // Check if stack is empty
+        if (isEmpty())
+        {
+            cout << "Stack is Empty." << endl;
+            return;
+        }
+
+        vec.pop_back();
+    }
+
+    // Return top element of stack
+    int top()
+    {
+        // Check if stack is empty
+        if (isEmpty())
+        {
+            cout << "Stack is Empty." << endl;
+            return -1;
+        }
+
+        int lastIdx = vec.size() - 1;
+        return vec[lastIdx];
+    }
+
+    // Check stack is empty or not
+    bool isEmpty()
+    {
+        return vec.size() == 0;
+    }
 };
 
 int main()
 {
-	
-	Stack s;
-	
-	s.push(3);
-	s.push(2);
-	s.push(1);
-	
-	while(!s.isEmpty())
-	{
-		cout<<s.top()<<" "<<endl;
-		s.pop();
-	}
-	
-	return 0;
+    Stack s;
+
+    // Inserting elements into stack
+    s.push(3);
+    s.push(2);
+    s.push(1);
+
+    // Printing and removing elements
+    while (!s.isEmpty())
+    {
+        cout << s.top() << endl;
+        s.pop();
+    }
+
+    return 0;
 }
