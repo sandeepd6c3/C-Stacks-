4e26bfe9ee034505b3fe7c2e1056282a63f33893
[Sandeep] <sandeep977244@gmail.com>
2026-05-30 06:34:47 +0530

    Push at bottom in a stack using recursion.

---
+++ PushAtBottomInStack.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+
+void PushAtBottom(stack<int> &s , int val)
+{
+	if(s.empty())
+	{
+		s.push(val);
+		return;
+	}
+	int temp = s.top();
+	s.pop();
+	PushAtBottom(s,val);
+	s.push(temp);
+}
+
+int main()
+{
+	
+	stack<int> s;
+	
+	s.push(3);
+	s.push(2);
+	s.push(1);
+	
+	PushAtBottom(s,100);
+	
+	while(!s.empty())
+	{
+		cout<<s.top()<<" ";
+		s.pop();
+	}
+	cout<<endl;
+	
+	return 0;
+}
